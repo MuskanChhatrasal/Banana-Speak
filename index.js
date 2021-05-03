59c5a1f4f3539c7309451fe1e010ddfe70752404
Muskan <muskanchhatrasal@gmail.com>
2021-05-03 22:22:59 +0530

    final commit

---
+++ index.js
@@ -0,0 +1,26 @@
+var txtinput = document.querySelector("#translate-input");
+var btn = document.querySelector("#translate-button");
+var txtoutput = document.querySelector("#output");
+
+
+var ServerUrl = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationUrl(input){
+    return ServerUrl + "?" + "text=" + input;
+}
+
+function clickHandler(){
+    // txtoutput.innerText = "gbsjdgjxf" + txtinput.value;
+    var inputTxt = txtinput.value; //taking input
+   
+    //calling server for processing
+
+    fetch(getTranslationUrl(inputTxt))
+      .then(response => response.json())
+      .then(json => {
+          var translatedtxt = json.contents.translated;
+          txtoutput.innerText = translatedtxt;//output
+      })
+}
+
+btn.addEventListener("click", clickHandler);
